d5edb281d44e81d2983fbffd6ecbb9d74be6a828
Raegan Van Raamsdonk <raeganvr7@gmail.com>
2025-04-01 00:07:39 -0700

    e

--- app/api/app.py
+++ app/api/app.py
@@ -23,7 +23,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",  # for local development
-        "https://interactive-ai.onrender.com"  # for production
+        "https://interactive-ai.onrender.com",  # for production
         "https://interactive-qhl9h682o-raeganvrs-projects.vercel.app"
 
     ],    
